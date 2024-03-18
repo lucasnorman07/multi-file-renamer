be0b31682b6a5e2468a41dbda68b2c0c432cc5fe
Lucas Norman <lucas.norman007@gmail.com>
2024-03-18 13:30:45 +0100

    Converted the File Controller class into a singelton class, and added methods to add prefixes and suffixes to the files in the current folder

--- App.java
+++ App.java
@@ -1,12 +1,23 @@
 import menu.Frame;
 import menu.Panel;
+
+import java.io.File;
+
 import file_renaming.*;
 
 public class App {
     public static void main(String[] args) {
         Panel mainPanel = new Panel();
         Frame mainFrame = new Frame("Multi File Renamer", mainPanel);
-        System.out.println(FileController.getBaseName("/mnt/home/myname/coolstuff/ctf/problems/forensics/flag.txt"));
+        FileController fileController = FileController.getInstance();
+        fileController.setFolderPath("folder");
+
+        fileController.printFileNamesInFolder();
+        fileController.addPrefixToFilesInCurrentFolder("prefix");
+        fileController.addSuffixToFilesInCurrentFolder("suffix");
+        fileController.printFileNamesInFolder();
+
+        // System.out.println(FileController.getBaseName("/mnt/home/myname/coolstuff/ctf/problems/forensics/flag.txt"));
         // boolean renameStatus = FileController.rename("file.txt", "renamed.txt");
         // if (renameStatus) System.out.println("RENAME SUCCESS!");
         // else System.out.println("RENAME FAILURE!");
--- file_renaming/FileController.java
+++ file_renaming/FileController.java
@@ -1,18 +1,69 @@
 package file_renaming;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class FileController {
-    public static String currentFolderPath;
+    private static FileController _instance;
+
+    private File currentFolder;
+
+    // to disable the default constructor
+    private FileController() {}
+
+    public static synchronized FileController getInstance() {
+        if (_instance == null) {
+            _instance = new FileController();
+        }
+        return _instance;
+    }
+
+    public void setFolderPath(String folderPath) {
+        currentFolder = new File(folderPath);
+    }
+    
+    public File[] getFileNamesInCurrentFolder() {
+        if (currentFolder == null) return null;
+        // if the folder is not null, then return the file paths
+        return currentFolder.listFiles();
+    }
+
+    public void addPrefixToFilesInCurrentFolder(String prefix) {
+        for (File file : currentFolder.listFiles()) {
+            rename(file, prefix + file.getName());
+        }
+    }
+
+    public void addSuffixToFilesInCurrentFolder(String suffix) {
+        for (File file : currentFolder.listFiles()) {
+            // split with \\. to match with the literal dot character because split uses regex
+            String[] sections = file.getName().split("\\.");
+            // calculate the new file name by adding the suffix before the first extension
+            String newFileName = sections[0] + suffix + Arrays.stream(sections, 1, sections.length).reduce("", (subtotal, element) -> subtotal + "." + element);
+            rename(file, newFileName);
+        }
+    }
+
+    public void printFileNamesInFolder() {
+        File[] files = getInstance().getFileNamesInCurrentFolder();
+        if (files == null) {
+            System.out.println("The current files in folder is null");
+        } else {
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        }
+    }
 
     // returns true for success and false for failure
-    public static boolean rename(String filePath, String newFilePath) {
-        File file = new File(filePath);
+    public static boolean rename(File file, String newFileName) {
+        String newFilePath = file.getParentFile().toPath() + "/" + newFileName;
         return file.renameTo(new File(newFilePath));
     }
 
-    public static String getBaseName(String filePath) {
-        String[] pathSections = filePath.split("/");
+    public static String getBaseName(File file) {
+        String[] pathSections = file.getName().split("/");
         return pathSections[pathSections.length - 1];
     }
 }
